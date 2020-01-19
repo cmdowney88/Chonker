a3e6fbbbef6e19e5030fd547b1b7cbe1e0a0e7d5
C.M. Downey <cmdowney@uw.edu>
2020-01-19 09:49:25 -0800

    Regularize line lengths in wrangle, fix typos

--- chonker/wrangle.py
+++ chonker/wrangle.py
@@ -28,7 +28,10 @@ def basic_tokenize(in_file, out_file=None):
     this file
     '''
     text = split_lines(get_lines(in_file))
-    text = [[word.lower() if not word.startswith('<') else word for word in line] for line in text]
+    text = [
+        [word.lower() for word in line] 
+        for line in text
+    ]
     if out_file:
         with open(out_file, 'w') as f:
             for line in text:
@@ -43,7 +46,10 @@ def character_tokenize(in_file, out_file=None):
     If an `out_file` is given print the tokenized text to this file
     '''
     text = split_lines(get_lines(in_file))
-    text = [list(itertools.chain.from_iterable([[char for char in word.lower()] for word in line])) for line in text]
+    text = [
+        flatten([[char for char in word.lower()] for word in line]) 
+        for line in text
+    ]
     if out_file:
         with open(out_file, 'w') as f: 
             for line in text:
@@ -76,6 +82,15 @@ class Vocab():
         self.processed_sources = []
         self.add_source(source)
 
+    def add_vocab(self, tokens):
+        '''Add vocabulary items directly as a string or list of strings'''
+        if type(tokens) != list: tokens = [tokens]
+        for token in tokens:
+            if token not in self.tok_to_id:
+                index = len(self.tok_to_id)
+                self.tok_to_id[token] = index
+                self.id_to_tok[index] = token
+
     def add_source(self, source):
         '''
         Iterate through the source texts in `to_tokenize`. If they have not 
@@ -86,31 +101,25 @@ class Vocab():
             for token in tokens:
                 self.add_vocab(token)
             self.processed_sources.append(source)
-        self.to_process = []
-
 
     def source_added(self, source):
         '''Return new copy of `Vocab` object with new source text added'''
-        new_voc = copy.deepcopy(self)
-        new_voc.add_source(files)
-        return new_voc
-
-    def add_vocab(self, tokens):
-        '''Add vocabulary items directly as a string or list of strings'''
-        if type(tokens) != list: tokens = [tokens]
-        for token in tokens:
-            if token not in self.tok_to_id:
-                index = len(self.tok_to_id)
-                self.tok_to_id[token] = index
-                self.id_to_tok[index] = token
+        new_vocab = copy.deepcopy(self)
+        new_vocab.add_source(source)
+        return new_vocab
 
     def reset(self):
         '''Completely empty all vocabulary items and source file lists'''
         self.tok_to_id = {}
         self.id_to_tok = {}
-        self.to_tokenize = []
-        self.tokenized_sources = []
-        self.source_texts = []
+        self.processed_sources = []
+
+    def size(self):
+        '''Return the size of the vocabulary (number of unique IDs)'''
+        size1 = len(self.tok_to_id)
+        size2 = len(self.id_to_tok)
+        assert size1 == size2
+        return size1
 
     def to_ids(self, tokens):
         '''
@@ -139,13 +148,6 @@ class Vocab():
             else:
                 output.append(self.id_to_tok[item])
         return output
-    
-    def size(self):
-        '''Return the size of the vocabulary (number of unique IDs)'''
-        size1 = len(self.tok_to_id)
-        size2 = len(self.id_to_tok)
-        assert size1 == size2
-        return size1
 
     def save(self, out_file, explicit_id=False):
         '''
@@ -163,4 +165,3 @@ class Vocab():
             else:
                 for item in items:
                     print(item[0], file=f)
-
