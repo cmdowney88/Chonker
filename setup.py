237345ecedef572b79a2e74d3a6d0a0413acdf19
C.M. Downey <cmdowney88@gmail.com>
2021-08-05 09:47:54 -0700

    v1.4.1: Fix setup.py to match version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='chonker',
-    version='1.4.0',
+    version='1.4.1',
     author="C.M. Downey",
     author_email="cmdowney@uw.edu",
     description="A utility package for NLP and Machine Learning",
